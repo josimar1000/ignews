4782629e1026bd92a9bd0dee74ee9c3c26a2146e
Josimar Lima <osimar.lima2007@gmail.com>
2021-09-04 00:26:00 -0300

    add header and SignInButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello <span>World</span></h1>
+    </>
   )
 }
